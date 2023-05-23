1829e3b9f70a4e0b18cbb3780d58a3095c2a5080
Facundo Lema <facurlema@gmail.com>
2023-05-23 02:24:38 -0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#FFFFFF',
+        grey: {
+          100: '#EEEEEE',
+          500: '#D9D9D9',
+          900: '#333333',
+        },
+        accent: {
+          100: '#FCEACF',
+          500: '#EEB969',
+          900: '#E09D38',
+        },
+      },
     },
   },
   plugins: [],
